5e39170fe15b13d0bc8bf8396d578a07bcbf2258
Francisco Marin perez <fmarin-p@c2r19s6.42madrid.com>
2023-01-12 17:46:43 +0100

    first setup

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fmarin-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:34:40 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/01/12 15:09:41 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:51:26 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <unistd.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fmarin-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:36:43 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/01/12 15:13:15 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/01/12 17:46:09 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 int	main(void)
 {
 	char	*line;
+	char	*abs_path;
 
+	abs_path = malloc(sizeof(char) * 1000);
 	while (1)
 	{
-		line = readline("minishell$ ");
-		if (*line)
+		getcwd(abs_path, 1000);
+		line = ft_strtrim(readline("minishell$ "), " ");
+		if (line && *line)
 			add_history(line);
-		if (!ft_strncmp(line, "exit", 4))
+		if (!ft_strncmp(line, "exit\0", 5))
 			break ;
+		else if (!ft_strncmp(line, "pwd\0", 4))
+			printf("%s\n", abs_path);
+		else if (!ft_strncmp(line, "cd\0", 3) || !ft_strncmp(line, "cd ", 3))
+			if (chdir(&line[3]))
+				perror("cd");
 		free(line);
+		ft_bzero(abs_path, 1000);
 	}
+	free(abs_path);
 	return (0);
 }
